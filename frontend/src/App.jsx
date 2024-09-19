bc0a4603e97a9327942e9ede9904329bd2681638
Pavan-Rajana <pkcoder.res@gmail.com>
2024-09-19 17:48:15 +0530

    Completed Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,25 @@
+import { Routes, Route } from "react-router-dom"
+import { Home, Collection, Cart, Contact, About, Login, Orders, PlaceOrder, Product } from "./paths"
+import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
-    <h1>App</h1>
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+
+      <Navbar /> {/*  Here mounting the navbar above the routes bcoz it should be available to all routes*/}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/product/:product:id" element={<Product />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+      </Routes>
+
+    </div>
   )
 }
 
--- frontend/src/assets/admin_assets/assets.js
+++ frontend/src/assets/admin_assets/assets.js
@@ -4,7 +4,7 @@ import order_icon from './order_icon.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.svg'
 
-export const assets = {
+export const a_assets = {
     logo,
     add_icon,
     order_icon,
