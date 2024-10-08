00deb02770f912abf6ae44023ec81d652953530b
Pavan-Rajana <pkcoder.res@gmail.com>
2024-10-08 10:17:29 +0530

    Main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,9 +5,10 @@ import './index.css'
 import ShopContextProvider from './context/ShopContext.jsx'
 
 createRoot(document.getElementById('root')).render(
+
   <BrowserRouter>
     <ShopContextProvider>
       <App />
     </ShopContextProvider>
-  </BrowserRouter>,
-)
+  </BrowserRouter>
+ )
