4a7469354bc153629a4216f286645b189b2e7eb2
Pavan-Rajana <pkcoder.res@gmail.com>
2024-10-27 09:43:05 +0530

    Footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,11 @@
+import { useContext } from "react"
 import { a_assets } from "../assets/admin_assets/assets"
+import { ShopContext } from "../context/ShopContext"
 
 const Footer = () => {
+
+    const { navigate } = useContext(ShopContext)
+
     return (
         <div>
             <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
@@ -12,11 +17,11 @@ const Footer = () => {
                 </div>
                 <div>
                     <p className="text-xl font-medium mb-5">COMPANY</p>
-                    <ul className="flex flex-col gap-1 text-gray-600">
-                        <li>Home</li>
-                        <li>About Us</li>
-                        <li>Delivery</li>
-                        <li>Privacy Policy</li>
+                    <ul className="flex flex-col gap-1 text-gray-600 cursor-pointer">
+                        <li onClick={() => navigate('/')}>Home</li>
+                        <li onClick={() => navigate('/about')}>About Us</li>
+                        <li onClick={() => navigate('/policy')}>Delivery</li>
+                        <li onClick={() => navigate('/policy')}>Privacy Policy</li>
                     </ul>
                 </div>
                 <div>
