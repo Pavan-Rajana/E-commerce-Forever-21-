1a598e665a5a5c5f0fe1d5a0dd2ac9aa5f17e780
Pavan-Rajana <pkcoder.res@gmail.com>
2024-11-13 18:37:36 +0530

    user Controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -92,4 +92,25 @@ export const registerUser = async (req, res) => {
 }
 
 // Route for admin login
-export const adminLogin = async (req, res) => {}
+export const adminLogin = async (req, res) => {
+	try {
+		const {email, password} = req.body
+
+		if (
+			email === process.env.ADMIN_EMAIL &&
+			password === process.env.ADMIN_PASSWORD
+		) {
+			const token = JWT.sign(email + password, process.env.JWT_SECRET)
+			res.json({success: "true", token})
+		} else {
+			res.json({success: "false", msg: "invalid Credentials"})
+		}
+	} catch (error) {
+		console.log(error)
+
+		res.json({
+			success: false,
+			msg: "Logging into your account has failed",
+		})
+	}
+}
