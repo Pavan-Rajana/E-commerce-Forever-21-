e2c4bcbe32a17893695c6cda17b8d1175350466a
Pavan-Rajana <pkcoder.res@gmail.com>
2024-12-31 21:23:36 +0530

    c

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -6,7 +6,7 @@ import CartTotal from "../components/CartTotal"
 
 const Cart = () => {
 
-    const { products, currency, cartItems, clearCart, updateQuantity, navigate } = useContext(ShopContext)
+    const { products, currency, cartItems, updateQuantity, navigate } = useContext(ShopContext)
     const [cartData, setCartData] = useState([])
 
     useEffect(() => {
