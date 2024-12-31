9a8d193058d2eee8351793780345e6ebfb52240b
Pavan-Rajana <pkcoder.res@gmail.com>
2024-12-31 21:26:16 +0530

    commit

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -6,7 +6,7 @@ import CartTotal from "../components/CartTotal"
 
 const Cart = () => {
 
-    const { products, currency, cartItems, updateQuantity, navigate } = useContext(ShopContext)
+    const { products, currency, cartItems, clearCart, updateQuantity, navigate } = useContext(ShopContext)
     const [cartData, setCartData] = useState([])
 
     useEffect(() => {
@@ -27,10 +27,10 @@ const Cart = () => {
         // console.log(tempData)
     }, [cartItems])
 
-    // const handleCheckout = () => {
-    //     clearCart()
-    //     navigate("/place-orders")
-    // }
+    const handleCheckout = () => {
+        clearCart()
+        navigate("/place-orders")
+    }
 
     return (
         <div className="border-t py-14">
@@ -64,7 +64,7 @@ const Cart = () => {
                 <div className="w-full sm:w-[450px]">
                     <CartTotal />
                     <div className="w-full text-end">
-                        <button onClick={() => navigate("/place-order")} className="bg-black text-white p-2 px-7 rounded-sm text-xs my-8 ">PROCEED TO CHECKOUT</button>
+                        <button onClick={handleCheckout} className="bg-black text-white p-2 px-7 rounded-sm text-xs my-8 ">PROCEED TO CHECKOUT</button>
                     </div>
                 </div>
             </div>
